7dafd466949bd3eeeefdfe4e0891adec7b89d45d
* <>
2026-02-05 15:16:41 +0100

    retry failed fetches

--- HNreader/Model/NewsItemModel.swift
+++ HNreader/Model/NewsItemModel.swift
@@ -11,7 +11,9 @@ import UIKit
 @Observable class NewsItemModel: BaseModel {
     var feedType: FeedType = .top
     var newsItem: NewsItem?
-    
+    var retryCounter: [Int: Int] = [:]
+    static var retryCounterMax = 3
+
     override init() {
         #if os(watchOS)
         Self.queue.maxConcurrentOperationCount = 4
@@ -25,7 +27,15 @@ import UIKit
                     self.newsItem = newsItem
                 } else {
                     Logger.shared.log(level: .default, "decode news item failed: \(String(data: data, encoding: .utf8) ?? "empty")")
-                    print("decode news item failed")
+                    if let counter = self.retryCounter[id] {
+                        if counter < Self.retryCounterMax {
+                            self.fetch(id: id)
+                        } else {
+                            Logger.shared.log(level: .default, "decode news item failed \(Self.retryCounterMax) times, not retrying")
+                        }
+                    } else {
+                        self.fetch(id: id)
+                    }
                 }
             })
         }
